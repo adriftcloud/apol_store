e15d69c2c9411ff0b0e81204c6de8c4f57aa2996
niz <niz.tseng@gmail.com>
2018-01-06 09:38:19 +0800

    init

---
+++ application/views/welcome_message.blade.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>巨福關係企業</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="./assets/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/libs/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="./assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid" id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <p class="textTitle">巨福關係企業</p>
+                    <p class="textTitle2">不動產買賣租賃/仲介代銷/營建設計/代書業務/法拍查封/貨櫃屋/包裝材料</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a id="" class="go_home btn btn-link visible-xs" href="page1.html">
+                    <img src="./assets/images/index/gohome.png" width="46" height="38"/>
+                </a>
+            </div>
+
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active hidden-xs">
+                        <a href="page1.html">
+                            <img src="./assets/images/index/gohome.png" height="20"/>
+                        </a>
+                    </li>
+                    <li><a href="page1.html">Page1</a></li>
+                    <li><a href="page2.html">Page2</a></li>
+                    <li><a href="page3.html">Page3</a></li>
+                    <li><a href="page4.html">Page4</a></li>
+                    <li><a href="page5.html">Page5</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <div class="container m-b-20">
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <h1 id="top_title">巨福關係企業</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row main_btn">
+            <div class="col-xs-6 m-b-10">
+                <button class="btn btn-warning btn-block btn-lg">買屋</button>
+            </div>
+            <div class="col-xs-6 m-b-10">
+                <button class="btn btn-warning btn-block btn-lg">租屋</button>
+            </div>
+            <div class="col-xs-6 m-b-10">
+                <button class="btn btn-warning btn-block btn-lg">營建設計</button>
+            </div>
+            <div class="col-xs-6 m-b-10">
+                <button class="btn btn-warning btn-block btn-lg">代書業務</button>
+            </div>
+            <div class="col-xs-6 m-b-10">
+                <button class="btn btn-warning btn-block btn-lg">貨櫃房屋</button>
+            </div>
+            <div class="col-xs-6 m-b-10">
+                <button class="btn btn-warning btn-block btn-lg">包裝材料</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row" id="horseT">
+            <img src="./assets/images/index/001_26.png" alt="" width="123" height="112"/>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="footer_nav">
+                <ul>
+                    <li>關於巨福 │</li>
+                    <li>服務據點 │</li>
+                    <li>經銷加盟 │</li>
+                    <li>客服專線</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="footer">
+                gf all rights reserved|power by gf
+            </div>
+        </div>
+    </div>
+
+</body>
+
+<script src="./assets/libs/jquery-1.12.4.min.js"></script>
+<script src="./assets/libs/bootstrap/js/bootstrap.min.js"></script>
+
+</html>
